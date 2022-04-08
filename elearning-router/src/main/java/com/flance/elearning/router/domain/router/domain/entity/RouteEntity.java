dd5da616724f0a3cd534371781a457e65d47d8ff
jianghf41372 <18151681814Jhf>
2022-04-08 10:39:49 +0800

    [新增]初始化

--- elearning-router/src/main/java/com/flance/elearning/router/domain/router/domain/entity/RouteEntity.java
+++ elearning-router/src/main/java/com/flance/elearning/router/domain/router/domain/entity/RouteEntity.java
@@ -4,6 +4,8 @@ package com.flance.elearning.router.domain.router.domain.entity;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.flance.jdbc.mybatis.common.BaseEntity;
 import com.flance.saas.db.annotation.Column;
+import com.flance.saas.db.annotation.Table;
+import com.flance.saas.db.tables.common.BaseTable;
 import com.flance.web.utils.route.RouteModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +14,8 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @TableName("sys_flance_router")
-public class RouteEntity extends BaseEntity<String> implements RouteModel {
+@Table(tableName = "sys_flance_router")
+public class RouteEntity extends BaseTable implements RouteModel {
 
     @Column(notNull = true, length = "64")
     private String routeId;
