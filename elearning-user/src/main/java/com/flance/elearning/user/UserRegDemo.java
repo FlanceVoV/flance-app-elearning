a29c9debb5814a3c43f040c891329412e1c03886
jianghf41372 <18151681814Jhf>
2022-04-11 19:08:13 +0800

    [新增]初始化

---
+++ elearning-user/src/main/java/com/flance/elearning/user/UserRegDemo.java
@@ -0,0 +1,25 @@
+package com.flance.elearning.user;
+
+import com.alibaba.nacos.common.utils.MD5Utils;
+import com.flance.elearning.user.domain.LoginVo;
+import com.flance.saas.tenant.domain.user.domain.vo.LoginUser;
+import com.flance.web.utils.GsonUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserRegDemo {
+
+    public static void main(String[] args) {
+        String accountName = "test2";
+        String password = "123456";
+        String passwordEncode = MD5Utils.md5Hex(password, "UTF-8");
+        System.out.println(passwordEncode);
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        password = encoder.encode(accountName + passwordEncode);
+        LoginVo loginVo = new LoginVo();
+        loginVo.setUserAccount(accountName);
+        loginVo.setUserPassword(password);
+        System.out.println(GsonUtils.toJSONString(loginVo));
+    }
+
+}
