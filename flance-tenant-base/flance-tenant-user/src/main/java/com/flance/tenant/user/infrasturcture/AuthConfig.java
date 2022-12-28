a7493b5ac4197f92303e50c69482e1a9812ef982
Flance <jianghuafei12@163.com>
2022-12-28 15:25:20 +0800

    [修改]日志链路追踪

---
+++ flance-tenant-base/flance-tenant-user/src/main/java/com/flance/tenant/user/infrasturcture/AuthConfig.java
@@ -0,0 +1,18 @@
+package com.flance.tenant.user.infrasturcture;
+
+import com.flance.web.feign.FeignUser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthConfig {
+
+    @Bean
+    public FeignUser feignUser() {
+        FeignUser feignUser = new FeignUser();
+        feignUser.setUser("user");
+        feignUser.setPass("pass");
+        return feignUser;
+    }
+
+}
